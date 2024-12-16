00b4e8d8544f55530543d35f73083b18fcd05ea0
Rotem Horesh <horesh.rotem@gmail.com>
2024-12-16 11:32:38 +0200

    107 current  2024-12-16 11:32:34  25.05.20241201.ac35b10  6.6.63                          *

--- home/hyprland/input.nix
+++ home/hyprland/input.nix
@@ -1,7 +1,12 @@
 {
   wayland.windowManager.hyprland.settings = {
     input = {
+      kb_layout = "us,he";
+      kb_variant = ",qwerty";
+
       kb_options = [
+        "grp:alt_shift_toggle"
+
         "caps:swapescape"
       ];
 
